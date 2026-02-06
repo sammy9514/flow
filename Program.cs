6a6ee5eaba4f51e5b673eaad23ef88c283109c36
Ayomide <ogunyemiayomide700@gmail.com>
2026-02-06 11:21:05 +0000

    quotes updated uses quotes api

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddScoped<HttpClient>();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
